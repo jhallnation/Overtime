3689dffca0d347f6be8c8274682421135f357a13
Jason Hall <hall.jasondaniel@gmail.com>
2017-03-22 13:21:21 -0600

    inplemented user connection with tests

--- README.md
+++ README.md
@@ -4,8 +4,8 @@
 
 Models
 - Post -> date:date rationale:text
-- User -> Devise
-- AdminUser -> STI
+x User -> Devise
+x AdminUser -> STI
 
 ## Features:
 - Approval Workflow
@@ -16,3 +16,6 @@ Models
 
 ## UI:
 - Bootstrap -> formatting
+
+## Refactor TODOS:
+- Refactor user association integration test in post_spec
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -15,6 +15,8 @@ describe 'navigate' do
 
   describe 'creation' do
     before do 
+      user = User.create(email: "tester@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: 'jon', last_name: 'snow')
+      login_as(user, :scope => :user)
       visit new_post_path
     end
 
@@ -25,10 +27,17 @@ describe 'navigate' do
     it 'can be created from new form page' do 
       fill_in 'post[date]', with: Date.today
       fill_in 'post[ratinale]', with: 'some ratinale'
-
       click_on 'save'
 
       expect(page).to have_content('some ratinale')
+    end
+
+    it 'will have a user associated it' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[ratinale]', with: 'User Association'
+      click_on 'save'
+
+      expect(User.last.posts.last.ratinale).to eq('User Association')
     end
   end
 end
